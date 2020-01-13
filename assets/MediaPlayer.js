3b97999e0a186cf85be55f9a67407f2a8535ceb5
NicoleVenachi <ludwing@unicauca.edu.co>
2020-01-13 13:24:30 -0500

    Cap2_Plugins

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,9 +1,32 @@
 function MediaPlayer(config) { //objeto de configuracion
   this.media = config.el //el es la media del objeto
+  this.plugins = config.plugins || [] //pluginns llegan por obj de configuracion. this igual al plugin o a array vacio, para cuando no  hay plugin
+
+  this._initPlugins()//inicializar plugins
 }
-// MediaPlayer.prototype.isPaused = function() {
-//   return this.media.paused
-// } //Metodo para saber si el video esta pausado o detenido
+
+MediaPlayer.prototype._initPlugins = function() {
+  //hace algo con cada plugin definido, los corre
+  this.plugins.forEach(plugin => plugin.run(this)) //recibe y lo ejecuta, corre
+  //no se puede simplemente instanciar el plugin i.e: plugin(), se debe llamar algún método en particular
+}
+
+MediaPlayer.prototype.mute = function() {
+  this.media.muted = true
+}
+MediaPlayer.prototype.unmute = function() {
+  this.media.muted = false
+}
+MediaPlayer.prototype.toggleAudio = function() {
+  if(this.media.muted === true){
+    this.unmute()
+  }
+  else {
+    this.mute()
+  }
+}
+
+
 MediaPlayer.prototype.play = function() {
   this.media.play()
 }  //metodo play al video
@@ -18,5 +41,6 @@ MediaPlayer.prototype.toggleVideo = function () {
     this.media.pause()
   }//si esta corriendo, pause
 }
+
 
 export default MediaPlayer
--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,15 @@
 import MediaPlayer from './MediaPlayer.js'
-
+import AutoPlay from './plugins/AutoPlay.js'
 const video  = document.querySelector('video')
-const button = document.querySelector('button')
+const playButton = document.getElementById('play')
+const muteButton = document.getElementById('mute')
+const player = new MediaPlayer({
+  el: video,
+  plugins: [
+  //new AutoPlay()
+  ]
+})
+//el plugin es una instancia de AutoPlay
 
-const player = new MediaPlayer({el: video})
-
-button.onclick = () => player.toggleVideo()  //metodo correr un HTML MediaElement
+playButton.onclick = () => player.toggleVideo()
+muteButton.onclick = () => player.toggleAudio()
